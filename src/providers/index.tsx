29792caf1308d35e5bc482e34ec40a6968aa0cc1
Bruno Tetzner <brunoltetzner@gmail.com>
2022-01-27 18:39:59 -0300

    fix: useUserPlants added to index.js of src

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 import { ChakraProvider } from "@chakra-ui/provider";
 import { theme } from "../styles/theme";
 import { PlantsProvider } from "./plantsProvider";
-
+import { UserPlantsProvider } from "./userPlantsProvider";
 interface ProvidersProps {
   children: ReactNode;
 }
@@ -10,7 +10,9 @@ interface ProvidersProps {
 export function Providers({ children }: ProvidersProps) {
   return (
     <ChakraProvider theme={theme}>
-      <PlantsProvider>{children}</PlantsProvider>
+      <PlantsProvider>
+        <UserPlantsProvider>{children}</UserPlantsProvider>
+      </PlantsProvider>
     </ChakraProvider>
   );
 }
