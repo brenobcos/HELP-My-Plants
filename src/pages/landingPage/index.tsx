b63b02c2d7189480d10280ec4023dac093a33047
Bruno Tetzner <brunoltetzner@gmail.com>
2022-01-27 16:50:16 -0300

    created text and image  of landing page

--- src/pages/landingPage/index.tsx
+++ src/pages/landingPage/index.tsx
@@ -1,5 +1,30 @@
+import { Flex, Button, Text, Heading, Image } from "@chakra-ui/react";
+// import img from "./assets/landingpagelarge.png";
+const img = require("../../assets/landingpagelarge.png");
 function LandingPage() {
-  return <div>LandingPage</div>;
+  return (
+    <Flex w="100vw" h="100vh" justifyContent="center" flexDirection="column">
+      <Flex flexDirection="column" w="30%">
+        <Heading marginBottom="5%" fontSize="32px">
+          MANTENHA SUAS PLANTAS SAÚDAVEIS
+        </Heading>
+        <Text>
+          Plantas reduzem o nível de stress e melhoram seu humor - O que as
+          tornam perfeitas para sua casa e escritório.
+        </Text>
+        <Button
+          w="190px"
+          h="70px"
+          borderRadius="10px"
+          color="gray.0"
+          background="green.1000"
+        >
+          Cadastrar
+        </Button>
+      </Flex>
+      <Image src={img} />
+    </Flex>
+  );
 }
 
 export default LandingPage;
