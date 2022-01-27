f16b40b573aff8ff8b272de947130e08e0b443c1
Luiza Schmidt Mescolotto <lsmescolotto@gmail.com>
2022-01-27 14:36:28 -0300

    feat: plants provider

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,11 +1,16 @@
 import { ReactNode } from "react";
 import { ChakraProvider } from "@chakra-ui/provider";
 import { theme } from "../styles/theme";
+import { PlantsProvider } from "./plantsProvider";
 
 interface ProvidersProps {
   children: ReactNode;
 }
 
 export function Providers({ children }: ProvidersProps) {
-  return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
+  return (
+    <ChakraProvider theme={theme}>
+      <PlantsProvider>{children}</PlantsProvider>
+    </ChakraProvider>
+  );
 }
