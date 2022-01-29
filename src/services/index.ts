0acdfa83538bafa458452023328af0970b13c575
Bruno Tetzner <brunoltetzner@gmail.com>
2022-01-28 21:24:48 -0300

    fix: https protocol added to api

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://help-my-plants-backend.herokuapp.com/",
+  baseURL: "https://help-my-plants-backend.herokuapp.com/",
 });
