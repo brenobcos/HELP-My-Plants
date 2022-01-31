947ebfe7f4a4844b5e622de24b5294a92fdc0aca
Luiza Schmidt Mescolotto <lsmescolotto@gmail.com>
2022-01-31 11:01:01 -0300

    feat: add dashboard conditional render for when the user does not have plants in their personal dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -3,7 +3,8 @@ import { CardDashboard } from "../../components/CardDashboard";
 import { useUserPlants } from "../../providers/userPlantsProvider";
 import { useAuth } from "../../providers/auth";
 import { useEffect } from "react";
-import { Box, Flex } from "@chakra-ui/react";
+import { Flex, Text } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 interface plantMinMax {
   min: number;
@@ -40,22 +41,50 @@ function Dashboard() {
       firstLink="/plants"
       firstText="Encontrar uma nova planta"
     >
-      <Flex w="900px" h="500px" overflowX="scroll" position="fixed" bottom="5px" gap="25px">
-        {userPlants.map((plant: plant) => (
-          <CardDashboard
-            key={plant.id}
-            name={plant.name}
-            cientific_name={plant.cientific_name}
-            height={plant.height}
-            image={plant.image}
-            info={plant.info}
-            lighting={plant.lighting}
-            temperature={plant.temperature}
-            water={plant.water}
-            last_watering={plant.last_watering}
-          />
-        ))}
-      </Flex>
+      {userPlants.length >= 1 ? (
+        <Flex
+          w="900px"
+          h="500px"
+          overflowX="scroll"
+          position="fixed"
+          bottom="5px"
+          gap="25px"
+        >
+          {userPlants.map((plant: plant) => (
+            <CardDashboard
+              key={plant.id}
+              name={plant.name}
+              cientific_name={plant.cientific_name}
+              height={plant.height}
+              image={plant.image}
+              info={plant.info}
+              lighting={plant.lighting}
+              temperature={plant.temperature}
+              water={plant.water}
+              last_watering={plant.last_watering}
+            />
+          ))}
+        </Flex>
+      ) : (
+        <Flex
+          flexDirection="column"
+          h="30vh"
+          justify="space-evenly"
+          mt={["15%", "7%", "3%", "3%"]}
+          w={["90vw", "70vw", "50vw"]}
+        >
+          <Text textAlign="center">
+            Você ainda não tem plantas no seu jardim.
+          </Text>
+          <Text textAlign="center">
+            Adicione novas plantas clicando{" "}
+            <Link to="/plants">
+              <b>aqui</b>
+            </Link>
+            .
+          </Text>
+        </Flex>
+      )}
     </DashboardsRender>
   );
 }
