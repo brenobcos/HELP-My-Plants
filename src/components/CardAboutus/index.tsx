7c07f5e353121eacc5cf6dec84f104068100adca
Johnny Gregório <johkker@gmail.com>
2022-02-02 18:08:56 -0300

    fix: corrected behaviour of devs cards

--- src/components/CardAboutus/index.tsx
+++ src/components/CardAboutus/index.tsx
@@ -1,6 +1,5 @@
-import { Flex, IconButton, Image, Text } from "@chakra-ui/react";
+import { Flex, IconButton, Image, Text, keyframes } from "@chakra-ui/react";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
-import { motion, AnimateSharedLayout, AnimatePresence } from "framer-motion";
 
 interface CardProps {
   name: string;
@@ -17,83 +16,98 @@ export function CardAboutus({
   linkedin,
   office,
 }: CardProps) {
+  const appear = keyframes`
+   0%   {opacity: 0;}
+  25%  {opacity: 0;}
+  50%  {opacity: 0.6;}
+  75%  {opacity: 0.8;}
+  100% {opacity: 1;}
+  
+  `;
   function Shared() {
+    const appearAnimation = `${appear} 1 1s linear forwards`;
+
     return (
-      <AnimateSharedLayout>
-        <Flex margin="4px 0">
-          <motion.div
-            style={{
-              display: "flex",
+      <Flex
+        sx={{
+          paddingBottom: "4px",
+          alignItems: "center",
+          justifyContent: "flex-start",
+        }}
+      >
+        <Flex
+          style={{
+            alignItems: "center",
+            borderRadius: "50px 0 50px 0",
+            border: "3px solid transparent",
+            background:
+              "linear-gradient(#FFFFFF, #FFFFFF) padding-box,linear-gradient(20deg, #FFFFFF 30%, #184A2C 50%,#FFFFFF 70%) border-box",
+          }}
+        >
+          <Flex
+            flexDir="column"
+            width="68px"
+            sx={{
+              transition: "width 1s",
+              overflow: "hidden",
+              maxHeight: "98px",
+              opacity: "0",
+              zIndex: 1,
+              ":hover": {
+                animation: `${appearAnimation}`,
+                width: ["298px", "348px", "398px"],
+              },
+              ":active": {
+                animation: `${appearAnimation}`,
+                width: ["298px", "348px", "398px"],
+              },
               alignItems: "center",
-              borderRadius: "50px 0 50px 0",
-              border: "3px solid transparent",
-              background:
-                "linear-gradient(#FFFFFF, #FFFFFF) padding-box,linear-gradient(15deg, #FFFFFF 30%, #184A2C 30%,#FFFFFF 90%) border-box",
+              justifyContent: "flex-start",
             }}
-            layout
           >
-            <Image
-              boxShadow="0 2px 4px 2px rgba(0, 0, 0, 0.15)"
-              width="98px"
-              maxWidth="98px"
-              height="98x"
-              maxHeight="98px"
-              border="3px solid transparent"
-              bg="linear-gradient(#FFFFFF, #FFFFFF) padding-box,linear-gradient(50deg, #FFFFFF 25%, #184A2C 70%,#FFFFFF 0%) border-box"
-              borderRadius="50px 0 50px 0"
-              src={image}
-              alt={name}
-            />
-            <AnimatePresence>
-              <motion.div
-                layout
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-              >
-                <Flex
-                  flexDir="column"
-                  width={["200px", "220px", "220px", "300px"]}
-                >
-                  <Text textAlign="center" color="green.800" fontSize={"20px"}>
-                    {name}
-                  </Text>
-                  <Text textAlign="center" color="green.500" fontSize="16px">
-                    {office}
-                  </Text>
-                  <Flex
-                    alignItems="center"
-                    justifyContent="space-evenly"
-                    margin="0 30px"
-                  >
-                    <IconButton
-                      as="a"
-                      _active={{ border: "none" }}
-                      _after={{ border: "none" }}
-                      _focus={{ border: "none" }}
-                      cursor="pointer"
-                      bgColor="transparent"
-                      aria-label=""
-                      onClick={() => (window.location.href = `${github}`)}
-                      icon={<FaGithub fontSize="25px" />}
-                    ></IconButton>
-                    <IconButton
-                      _active={{ border: "none" }}
-                      _after={{ border: "none" }}
-                      _focus={{ border: "none" }}
-                      cursor="pointer"
-                      bgColor="transparent"
-                      aria-label=""
-                      onClick={() => (window.location.href = `${linkedin}`)}
-                      icon={<FaLinkedin fontSize="25px" />}
-                    ></IconButton>
-                  </Flex>
-                </Flex>
-              </motion.div>
-            </AnimatePresence>
-          </motion.div>
+            <Text textAlign="center" color="green.800" fontSize={"20px"}>
+              {name}
+            </Text>
+            <Text textAlign="center" color="green.500" fontSize="16px">
+              {office}
+            </Text>
+            <Flex alignItems="center" justifyContent="center" margin="0 30px">
+              <IconButton
+                as="a"
+                _active={{ border: "none" }}
+                _after={{ border: "none" }}
+                _focus={{ border: "none" }}
+                cursor="pointer"
+                bgColor="transparent"
+                aria-label=""
+                onClick={() => (window.location.href = `${github}`)}
+                icon={<FaGithub fontSize="25px" />}
+                marginLeft="10%"
+              ></IconButton>
+              <IconButton
+                _active={{ border: "none" }}
+                _after={{ border: "none" }}
+                _focus={{ border: "none" }}
+                cursor="pointer"
+                bgColor="transparent"
+                aria-label=""
+                onClick={() => (window.location.href = `${linkedin}`)}
+                icon={<FaLinkedin fontSize="25px" />}
+              ></IconButton>
+            </Flex>
+          </Flex>
+          <Image
+            className="AvatarImage"
+            boxShadow="0 2px 4px 2px rgba(0, 0, 0, 0.15)"
+            width="98px"
+            height="98x"
+            borderRadius="50px 0 50px 0"
+            src={image}
+            alt={name}
+            position="absolute"
+          />
         </Flex>
-      </AnimateSharedLayout>
+      </Flex>
     );
   }
 
