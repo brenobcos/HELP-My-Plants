c6c4c154229b16b71166e764449ef9eb1b1abc57
Caique F. Cavalheiro <caiquefcavalheiro@gmail.com>
2023-04-17 13:08:50 -0300

    feat: change baseUrl

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://help-my-plants.azurewebsites.net/",
+  baseURL: "https://help-my-plants-backend.vercel.app/",
 });
